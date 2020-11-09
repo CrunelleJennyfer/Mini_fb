244841b30723f154341d1ce079a97f48ae9b3d02
CrunelleJennyfer <jennyfercrunelle62000@gmail.com>
2020-11-09 10:59:00 +0100

    Update index.php
    insertion de kiwi

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ ob_start(); // Je démarre le buffer de sortie : les données à afficher sont s
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ma super application</title>
+    <title>KiwiOn</title>
 
 
   
@@ -38,7 +38,7 @@ if (isset($_SESSION['info'])) {
 
 
 <header>
-    <h3>Le super site</h3>
+    <h3>Le super site à base de KIWI</h3>
 </header>
 <nav>
     <ul>
