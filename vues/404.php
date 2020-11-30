47d9ae768c7ae2f979be72f595a38ad55e293d40
Corentin <49951336+NicodemeCorentin@users.noreply.github.com>
2020-11-30 15:21:26 +0100

    CSS Index, 404,, accueil

--- vues/404.php
+++ vues/404.php
@@ -1,1 +1,11 @@
-Désolé, cette page est introuvable ! ~M.MV
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<a class="a404"> Désolé, cette page est introuvable ! </a>
+
+<img class="mvpic" src="img/mistermv">
+    
